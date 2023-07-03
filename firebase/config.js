617c5676758e6f194a24acdb3a2356cb4f54ad3d
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-07-03 23:32:10 +0300

    update

--- firebase/config.js
+++ firebase/config.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgAtNpZiqufKJ3mT61Q5alE1onHlDyrKw",
-  authDomain: "goit-react-native-8f12b.firebaseapp.com",
-  projectId: "goit-react-native-8f12b",
-  storageBucket: "goit-react-native-8f12b.appspot.com",
-  messagingSenderId: "451271538613",
-  appId: "1:451271538613:web:c40b3440138e270f474bfc",
-  measurementId: "G-QBFFJRFLP3",
+  apiKey: "AIzaSyABtKXMBVK0iH0OQW2MQswyhxRzd2u7aIQ",
+  authDomain: "projectnative-ca157.firebaseapp.com",
+  databaseURL: "https://projectnative-ca157-default-rtdb.firebaseio.com",
+  projectId: "projectnative-ca157",
+  storageBucket: "projectnative-ca157.appspot.com",
+  messagingSenderId: "121043770261",
+  appId: "1:121043770261:web:fdcf25e2212d821b3e349c"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export default app;
